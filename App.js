8da98c8925cecd80e31890e63e643553959de0fd
jypark <babungv@gmail.com>
2021-05-01 20:46:29 +0900

    #1.4 Getting the Weather

--- App.js
+++ App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "f8c97486460bc7bf47772aa38536addc";
 
 export default class extends React.Component {
   state = {
     isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    console.log(latitude, longitude)
+    const { data } = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+
   };
   getLocation = async () => {
     try {
       await Location.getForegroundPermissionsAsync();
       const { coords : { latitude, longitude }} = await Location.getCurrentPositionAsync();
       this.setState({isLoading: false})
+      this.getWeather(latitude, longitude);
     } catch (err) {
       Alert.alert("Cannot find you.", "So sad");
     }
--- README.md
+++ README.md
@@ -6,3 +6,16 @@
 * Xcode with Simulator
 * Expo
   * `npm install -g expo-cli`
+  
+# Get API 
+
+## Register
+https://home.openweathermap.org/
+
+## Get Json Data
+
+http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID={api_key}
+
+# Install Axios
+
+`npm install axios`
